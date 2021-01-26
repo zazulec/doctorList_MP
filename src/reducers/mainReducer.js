58408a68b8b87a4d675a33666fe207756a46906b
Paweł Zarzycki <pawelzarzycki@MacBook-Air-Pawe.local>
2021-01-27 00:36:03 +0100

    feat: prepared redux data for pagination

--- src/reducers/mainReducer.js
+++ src/reducers/mainReducer.js
@@ -1,12 +1,20 @@
-const initState = {};
+const initState = {
+    allTherapists: []
+};
 
 const mainReducer = (state = initState, action) => {
   let newState = state;
   switch (action.type) {
     case "FETCH_ALL_THERAPISTS":
+      let chunk;
+      let sortedData = [];
+      while (action.data.length > 0) {
+        chunk = action.data.splice(0, 9);
+        sortedData.push(chunk);
+      }
       newState = {
         ...state,
-        allTherapists: action.data,
+        allTherapists: sortedData,
       };
       break;
     default:
